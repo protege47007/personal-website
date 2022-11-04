09a1d1d0d6b31b06e7ccbd0281aaa2c6b33aa775
protege47007 <davidak.a@yahoo.com>
2022-11-04 16:54:58 +0100

    complete registration route

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,11 +1,11 @@
 function redirectIfLoggedIn(req, res, next){
-    if(req.user) return res.redirect("/user/account")
+    if(req.admin) return res.redirect("/dashboard")
     return next()
 }
 
-function isUserLoggedIn(req, res, next){
-    if (req.user) return next() // this is a middleware to check if a user exists
-    return res.status(401).end() // returns an error and ends the res cycle if the req obj has no user
-}
+// function isUserLoggedIn(req, res, next){
+//     if (req.user) return next() // this is a middleware to check if a user exists
+//     return res.status(401).end() // returns an error and ends the res cycle if the req obj has no user
+// }
 
-module.exports = { redirectIfLoggedIn, isUserLoggedIn }
+module.exports = { redirectIfLoggedIn }

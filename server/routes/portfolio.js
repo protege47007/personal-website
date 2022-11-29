27134bf680cdd9e77abee5dcd6434bafda00dcef
protege47007 <davidak.a@yahoo.com>
2022-11-29 18:18:50 +0100

    added db query

--- server/routes/portfolio.js
+++ server/routes/portfolio.js
@@ -1,26 +1,23 @@
 const express = require("express")
 const router = express.Router()
+const createError = require("http-errors")
+const findAll = require("../utilities/findAll")
+const portfolio = require("../models/portfolio")
 
 module.exports = ({portfolioService}) => {
     router.get("/", async (req, res, next) => {
         try {
             
-
+            // const projects = await findAll(portfolio)
             const projects = await portfolioService.getPortfolio()
             
             res.render('portfolio', {projects})
+
         } catch (error) {
-            return next(error)
+            return next(createError(500, {body: error, message: "internal server error"}))
         }
     })
 
-    router.post("/", async (req, res, next) => {
-        try {
-            
-        } catch (error) {
-            return next(error)
-        }
-    })
 
     return router
 }

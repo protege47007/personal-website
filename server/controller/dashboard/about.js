d81aa1e0f7c4e70b121e1a4596df273ab64bed2c
protege47007 <davidak.a@yahoo.com>
2022-11-29 18:08:27 +0100

    component for post-method about

--- server/controller/dashboard/about.js
+++ server/controller/dashboard/about.js
@@ -1,10 +1,25 @@
-router.get("/avatar/:filename", (req, res) => {
-    res.type("png"||"jpg")
-    return res.sendFile(avatars.filepath(req.params.filename))
-})
+const createError = require("http-errors")
 
-router.get("/avatartn/:filename", async (req, res) =>{
-    res.type("png")
-    const tn = await avatars.thumbnail(req.params.filename)
-    return res.end(tn, "binary")
-})
+module.exports = function (req, res, next){
+    try {
+        
+    } catch (error) {
+        return next(createError(500, {body: error, message: "internal server error"}))
+    }
+}
+
+
+
+
+
+
+// router.get("/avatar/:filename", (req, res) => {
+//     res.type("png"||"jpg")
+//     return res.sendFile(avatars.filepath(req.params.filename))
+// })
+
+// router.get("/avatartn/:filename", async (req, res) =>{
+//     res.type("png")
+//     const tn = await avatars.thumbnail(req.params.filename)
+//     return res.end(tn, "binary")
+// })

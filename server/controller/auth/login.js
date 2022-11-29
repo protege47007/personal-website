fdc4ae499c110dccc43ae23108c94b7afb2f0ed8
protege47007 <davidak.a@yahoo.com>
2022-11-29 18:06:36 +0100

    refactored to search for auth in the cookies of request headers

--- server/controller/auth/login.js
+++ server/controller/auth/login.js
@@ -1,20 +1,23 @@
 const Admin = require("../../models/about")
 const createError = require("http-errors")
 const { session } = require("../../config")[process.env.NODE_ENV || "development"]
+const { validationResult } = require("express-validator")
 const jwt = require("jsonwebtoken")
 
 
 module.exports = async (req, res, next) => {
     try {
+        
         const errors = validationResult(req)
             if(!errors.isEmpty()) return next(createError(403, {body: errors.array(), message: "Forbidden: Invalid Credentials"}))
-
+            
             const { mail, password } = req.body
+            
             Admin.findOne({ mail }, async (err, foundAdmin) => {
                 if (err) return next(createError(500, {body: err, message: "Internal Server Error"}))
-
+               
                 if(!foundAdmin) return next(createError(403, "Invalid Credentials"))
-
+                
                 const logged_in_admin = await foundAdmin.comparePassword(password)
 
                 if(!logged_in_admin) return next(createError(403, "Invalid Credentials"))
@@ -26,13 +29,15 @@ module.exports = async (req, res, next) => {
                     const ok = await foundAdmin.save()
                     if(!ok) return next(createError(500, {messgae: "internal server error"}))
                     
-                    res.status(200).json({body: token, message: "Login success"})
+                    req.signedCookie = token
+                    console.log("done", req.signedCookie) //log
                     res.redirect("/dashboard/")
                 })
                 
             })
 
     } catch (error) {
+        console.log("error:", error)
         return next(createError("500", { body: error, message: "internal server error"}))
     }
 }

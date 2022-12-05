35623f45de5925d6d62a811571a95d1407e4c6af
protege47007 <davidak.a@yahoo.com>
2022-12-05 18:05:03 +0100

    added resume router function

--- server/routes/dashboard/index.js
+++ server/routes/dashboard/index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const router = express.Router()
 const createError = require("http-errors")
 const about_controller = require("../../controller/dashboard/about")
+const resume_router = require("./resume")
+const portfolio_controller = require("../../controller/dashboard/portfolio")
 const About_Model = require("../../models/about")
 const Portfolio_Model = require("../../models/portfolio")
 const findAll = require("../../utilities/findAll")
@@ -17,8 +19,12 @@ module.exports = () => {
             const result = await findAll(About_Model)
             const { full_name, mail, job, dob, location, bio } = result[0]
             const aboutInfo = { full_name, mail, job, dob, location, bio }
+            
+            const profile_pic = ""
+
             res.render('dashboard/profile', { 
-                aboutInfo
+                aboutInfo,
+                profile_pic: false
              })
 
         } catch (error) {
@@ -30,24 +36,23 @@ module.exports = () => {
 
     router.post("/", about_controller)
 
-    router.get("/resume", async (req, res, next) => { //add validation middle wares
-        try {
-            const { full_name, dob, location, job } = req.body
-            
+    //resume dashboard route
+    router.use("/resume", resume_router())
 
-        } catch (error) {
-            return next(createError(500, {body: error, message: "internal server error"}))
-        }
-    } )
+    //portfolio dashboard route
     router.get("/portfolio", async (req, res, next) => { //add validation middle wares
         try {
-            const { full_name, dob, location, job } = req.body
+            const portfolio = await findAll(Portfolio_Model)
+
+            res.render("dashboard/portfolio", portfolio)
             
 
         } catch (error) {
             return next(createError(500, {body: error, message: "internal server error"}))
         }
-    } )
+    })
+
+    router.post("/portfolio", portfolio_controller)
     
     return router
 }

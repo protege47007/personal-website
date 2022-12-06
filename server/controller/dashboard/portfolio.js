49872434cf5f502dcaa9c0a50f8103a67d023291
protege47007 <davidak.a@yahoo.com>
2022-12-06 18:28:42 +0100

    started portfolio controller

--- server/controller/dashboard/portfolio.js
+++ server/controller/dashboard/portfolio.js
@@ -2,7 +2,11 @@ const createError = require("http-errors")
 
 module.exports = function (req, res, next){
     try {
-        
+        function find (name, query, cb) {
+            mongoose.connection.db.collection(name, function (err, collection) {
+               collection.find(query).toArray(cb);
+           });
+        }
     } catch (error) {
         return next(createError(500, {body: error, message: "internal server error"}))
     }

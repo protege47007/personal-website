4b4b22db979c52df43c92e6a1bd9bd52a4188b63
protege47007 <davidak.a@yahoo.com>
2022-12-13 18:31:39 +0100

    tailwind update

--- server/controller/dashboard/resume.js
+++
@@ -1,9 +0,0 @@
-const createError = require("http-errors")
-
-module.exports = function (req, res, next){
-    try {
-        
-    } catch (error) {
-        return next(createError(500, {body: error, message: "internal server error"}))
-    }
-}

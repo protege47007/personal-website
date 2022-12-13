f06aa433b5bd6c41f55e14e3b3a570014df89c8f
protege47007 <davidak.a@yahoo.com>
2022-12-13 18:34:51 +0100

    added an image stream link

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const router = express.Router()
+
+//error handler
 const createError = require("http-errors")
+
+//router functions
 const blogRoute = require("./blog")
 const contactRoute = require("./contact")
 const resumeRoute = require("./resume")
@@ -8,10 +12,13 @@ const portfolioRoute = require("./portfolio")
 const admin = require("./admin")
 const dashboard = require("./dashboard")
 
+//controller
+const {image_controller} = require("../services/GFS")
+
 //middlewares
 const logged_in = require("../middlewares/logged_in")
 
-// aboutServices is coming from params obj passed to the routes 
+//Services from app.js  
 module.exports = ({aboutService, portfolioService, logService}) => {
     
 
@@ -25,11 +32,15 @@ module.exports = ({aboutService, portfolioService, logService}) => {
         }
         
     })
+
+    router.get("/image/:file_name", image_controller)
+
     router.use("/blog", blogRoute())
     router.use("/resume", resumeRoute({aboutService}))
     router.use("/contact", contactRoute())
     router.use("/portfolio", portfolioRoute({portfolioService}))
     router.use("/adminxyz", admin({logService}))
+
     router.get("/fish", 
     function (req, res, next){
         console.log("fish test")
@@ -38,6 +49,8 @@ module.exports = ({aboutService, portfolioService, logService}) => {
     function(req, res, next){
         res.status(200).json({body: "fish data", message: "success"})
     })
+
+    
     router.use("/dashboard", logged_in, dashboard())
     
     return router

ceead586afb53b474011094f573095f8db9521a4
protege47007 <davidak.a@yahoo.com>
2022-12-13 18:36:14 +0100

    a proposed download link.. not functional

--- server/services/download.js
+++ server/services/download.js
@@ -1,6 +1,32 @@
 const mongoose = require("mongoose")
-const { GridFsBucket } = require("multer-gridfs-storage")
+const Grid = require("gridfs-stream")
+const mongoose = require("mongoose")
+Grid.mongo = mongoose.mongo
+// const { GridFsBucket } = require("multer-gridfs-storage")
 
-module.exports = async function (collection_name, file_name ){
-    const sm = new GridFsBucket({})
-}
+
+const db_connection = mongoose.connection
+
+db_connection.once("open", ()=>{
+  const gfs = Grid(db_connection.db)
+})
+
+router.delete("/:imageID", auth, async (req, res) => {
+    try {
+      const post = await Post.findOne({ image: req.params.imageID });
+      console.log(post);
+      if (post.user != req.user.id) {
+        res.status(401).send("Invalid credentials");
+      }
+  
+  // Here:
+      const obj_id = new mongoose.Types.ObjectId(req.params.imageID);
+      gfs.delete( obj_id );
+  
+      await post.remove();
+      res.json("successfully deleted image!");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  });

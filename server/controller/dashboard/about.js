f1847c18b22b5b8dcc7f76303efef8759025cd4d
protege47007 <davidak.a@yahoo.com>
2022-12-13 18:29:48 +0100

    modified the about controller to update with the image optional update condition

--- server/controller/dashboard/about.js
+++ server/controller/dashboard/about.js
@@ -1,11 +1,39 @@
+//error handler
 const createError = require("http-errors")
+const { validationResult } = require("express-validator")
+
+//models
 const About_Model = require("../../models/about")
 
-module.exports = function (req, res, next){
+//utility functions
+
+
+module.exports = async function (req, res, next){
     try {
-        let full_name
-        if(req.file.file_name){
-            // full_name = req.
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            const data = await JSON.stringify({body: errors.array(), message: "Forbidden: Invalid Credentials"})
+            res.cookie("state", data, { maxAge: 3e4 })
+            
+            return res.status(403).redirect("/dashboard/")
+        }
+
+        const update = {
+            full_name: req.body.full_name,
+            job: req.body.job,
+            location: req.body.location,
+            bio: req.body.bio
+        }
+
+        if(req.file){
+            
+            update["profile_pic"] = req.file.filename
+        }
+        
+        const result = await About_Model.findOneAndUpdate({ mail: process.env.MAIL }, update, { new: true })
+
+        if(result){
+            res.redirect("/dashboard/")
         }
         
     } catch (error) {

3812c12d4dffc4a4f48de33369ffc1edc230056b
protege47007 <davidak.a@yahoo.com>
2022-12-14 18:04:23 +0100

    added the resume onclick functionality

--- server/public/scripts/resume.js
+++ server/public/scripts/resume.js
@@ -1,3 +1,15 @@
-<script>let myVar = <%- JSON.stringify(myVar) %>;</script>
+// <script>let myVar = <%- JSON.stringify(myVar) %>;</script>
 // stringify the data passed from router to ejs (within the EJS template only)
+function $(id){
+    return document.querySelector(id)
+}
+const img = $("#preview_img")
+const txt = $("#preview_text")
+const search = $("#search_term")
+
+function preview(image_url, title){
+    img.setAttribute("src", `/image/${image_url}`)
+    txt.setAttribute("value", title)
+    search.setAttribute("value", image_url)
+}
 

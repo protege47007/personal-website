b79605c59aa50fe6a90f8216bffde7afe00c3331
protege47007 <davidak.a@yahoo.com>
2023-01-09 18:14:52 +0100

    updated constraints

--- server/middlewares/profile_constraints.js
+++ server/middlewares/profile_constraints.js
@@ -2,9 +2,11 @@ const { check } = require("express-validator")
 
 const profile_constraints = [
     check("full_name").trim().isLength({min: 3}).escape().withMessage("full name is unsatisfactory"),
-    check("bio").trim().isLength({min: 20}).withMessage("bio is unsatisfactory").matches(/^[A-Za-z0-9 .,'!&]+$/),
+    check("bio").trim().isLength({min: 20}).withMessage("bio is unsatisfactory"),
     check("location").trim().isLength({min: 3}).escape().withMessage("location is unsatisfactory"),
-    check("job").trim().isLength({min: 3}).escape().withMessage("job is unsatisfactory")
+    check("job").trim().isLength({min: 3}).escape().withMessage("job is unsatisfactory"),
+    check("dob").trim().isLength({min: 3}).escape().withMessage("date of birth is unsatisfactory"),
+    check("mail").trim().isEmail().normalizeEmail().withMessage("A valid email is required"),
 ]
 
 module.exports = profile_constraints
